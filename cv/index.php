5bae3d2c8259f0cd6d18069e626a06542662ae13
Alasdair Smith <alasdairsmith100@gmail.com>
2015-02-08 17:15:52 +0000

    Fix grammar on automated testing line

--- cv/index.php
+++ cv/index.php
@@ -90,7 +90,7 @@
 						<li>Working with others in a software team, using git as a core workflow</li>
 						<li>Communicating with others to organise and prioritise feature requests 
 						and bug reports</li>
-						<li>Used automated testing to develop and improve code</li>
+						<li>Using automated testing to develop and improve code</li>
 						<li>Developing for a scaleable RESTful API with a command bus architecture</li>
 						<li>Using modern web technologies to develop a Javascript client</li>
 						<li>Assisted in server maintenance and devops, and used Ansible to test 

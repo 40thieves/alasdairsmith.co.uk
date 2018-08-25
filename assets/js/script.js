764e61fb37cf07f1573efe4a9a873db50e74abe1
Alasdair Smith <ali@alasdairsmith.co.uk>
2018-08-25 16:50:10 +0100

    Rename and unify script

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('/sw.js')
+      .then(function () {
+        console.log('SW registration successful')
+      })
+      .catch(function () {
+        console.log('SW registration error')
+      })
+  })
+}
+
+console.log('   __ __  ____  __  __    _')
+console.log('  / // / / __ \\/ /_/ /_  (_)__ _   _____  _____')
+console.log(' / // /_/ / / / __/ __ \\/ / _ \\ | / / _ \\/ ___/')
+console.log('/__  __/ /_/ / /_/ / / / /  __/ |/ /  __(__  )')
+console.log('  /_/  \\____/\\__/_/ /_/_/\\___/|___/\\___/____/')

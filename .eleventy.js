3ccd1171dbae5cde6807d0c941e2f74eea206c99
Alasdair Smith <ali@alasdairsmith.co.uk>
2019-10-16 23:00:54 +0100

    Wrap keys in kbd

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/img");
 
+  eleventyConfig.addHandlebarsHelper("join", (array, sep, options) =>
+    array.map(item => options.fn(item)).join(sep)
+  );
+
   return {
     dir: {
       input: "src",

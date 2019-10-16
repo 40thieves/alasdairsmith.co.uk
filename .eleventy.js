7e33bc90677e5a6f06e61ecf7ff15fa9407d5a44
Alasdair Smith <ali@alasdairsmith.co.uk>
2019-10-16 23:01:43 +0100

    Prettify how I like it

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,15 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/img");
+  eleventyConfig.addPassthroughCopy('src/img')
 
-  eleventyConfig.addHandlebarsHelper("join", (array, sep, options) =>
+  eleventyConfig.addHandlebarsHelper('join', (array, sep, options) =>
     array.map(item => options.fn(item)).join(sep)
-  );
+  )
 
   return {
     dir: {
-      input: "src",
-      output: "dist",
-      templateFormats: ["hbs", "css"]
+      input: 'src',
+      output: 'dist',
+      templateFormats: ['hbs', 'css']
     }
-  };
-};
+  }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,26 @@
 module.exports = {
   theme: {
     fontFamily: {
-      sans: ["Roboto", "Helvetica", "Arial", "sans-serif"],
-      serif: ["Merriweather", "serif"]
+      sans: ['Roboto', 'Helvetica', 'Arial', 'sans-serif'],
+      serif: ['Merriweather', 'serif']
     },
     extend: {
       colors: {
-        "pastel-green": "hsla(85, 54%, 51%, 0.2)",
-        "pastel-teal": "hsla(173, 77%, 37%, 0.2)",
-        "pastel-blue": "hsla(204, 77%, 44%, 0.2)",
-        "pastel-purple": "hsla(326, 52%, 40%, 0.2)"
+        'pastel-green': 'hsla(85, 54%, 51%, 0.2)',
+        'pastel-teal': 'hsla(173, 77%, 37%, 0.2)',
+        'pastel-blue': 'hsla(204, 77%, 44%, 0.2)',
+        'pastel-purple': 'hsla(326, 52%, 40%, 0.2)'
       }
     }
   },
   plugins: [
-    require("tailwindcss-grid")({
+    require('tailwindcss-grid')({
       gaps: {
         0: 0,
-        1: "0.25rem",
-        2: "0.5rem",
-        4: "1rem"
+        1: '0.25rem',
+        2: '0.5rem',
+        4: '1rem'
       }
     })
   ]
-};
+}

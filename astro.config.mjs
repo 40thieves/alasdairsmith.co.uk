3327be167029e2de6662c10a7ae4ea25bde2c3d0
Alasdair Smith <ali@alasdairsmith.co.uk>
2026-01-03 18:08:04 +0000

    Add preact integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config'
 
+import preact from '@astrojs/preact'
+
 export default defineConfig({
+  integrations: [preact()],
+
   // Doesn't seem that useful to me, and injects a bunch of weight into the page
   devToolbar: {
     enabled: false

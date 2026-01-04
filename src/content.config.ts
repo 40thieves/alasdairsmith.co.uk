ce61ebe19969fdc4e88cd02da6cd62c922d759c1
Alasdair Smith <ali@alasdairsmith.co.uk>
2026-01-04 16:04:42 +0000

    Rename to journal
    This leaves room for "notes" being shorter (i.e. social media) posts

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,12 +2,12 @@ import { glob } from 'astro/loaders'
 import { z } from 'astro/zod'
 import { defineCollection } from 'astro:content'
 
-const notes = defineCollection({
-  loader: glob({ pattern: '**/*.md', base: './src/notes' }),
+const journalEntries = defineCollection({
+  loader: glob({ pattern: '**/*.md', base: './src/journal' }),
   schema: z.object({
     title: z.string(),
     'published-date': z.coerce.date()
   })
 })
 
-export const collections = { notes }
+export const collections = { journalEntries }

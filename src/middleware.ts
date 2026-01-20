4b9a191541762ff0e6948379855d9c49c7235327
Alasdair Smith <ali@alasdairsmith.co.uk>
2026-01-20 22:18:20 +0000

    Rename with service name prefix

--- src/middleware.ts
+++ src/middleware.ts
@@ -28,8 +28,8 @@ export const onRequest = defineMiddleware(async (context, next) => {
   const gridAware = new GridAware({
     session: context.session as GridAwareSession<string>,
     apiCredentials: {
-      username: import.meta.env.API_USERNAME,
-      password: import.meta.env.API_PASSWORD
+      username: import.meta.env.WATTTIME_API_USERNAME,
+      password: import.meta.env.WATTTIME_API_PASSWORD
     },
     getGeo: () => {
       // Allow overriding via DEV_GEO env var

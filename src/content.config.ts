735a6e356d1fe86c9b8fdce6766a3055502d8091
Alasdair Smith <ali@alasdairsmith.co.uk>
2026-05-03 22:11:10 +0100

    Document the possible file structure

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,6 +3,12 @@ import { z } from 'astro/zod'
 import { defineCollection } from 'astro:content'
 
 const journalEntries = defineCollection({
+  // Folder structure: either md files in root or nested inside named folders
+  // journal
+  // ├── blog-post-1.md
+  // └── blog-post-2
+  //     ├── blog-post-2.md
+  //     └── img-1.png
   loader: glob({ pattern: '**/*.md', base: './src/content/journal' }),
   schema: z.object({
     title: z.string(),

c7e2f7b2eef30ec97ef9398e877fbd16a3391dab
Alasdair Smith <ali@alasdairsmith.co.uk>
2026-05-03 10:02:52 +0100

    Switch to server rendering globally
    Server rendering is required for the grid-aware middleware to work.
    Previously was setting it on a per-route basis but have decided to just
    set it universally.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,10 @@ export default defineConfig({
   integrations: [preact()],
   adapter: netlify(),
 
+  // Disable static generation, use server rendering instead. This allows the
+  // grid-aware middleware to always work
+  output: 'server',
+
   // Doesn't seem that useful to me, and injects a bunch of weight into the page
   devToolbar: {
     enabled: false

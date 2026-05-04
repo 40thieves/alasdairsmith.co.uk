4243cd345e9569ce6f6b222d9075e187a665770c
Alasdair Smith <ali@alasdairsmith.co.uk>
2026-05-04 10:04:14 +0100

    Account for nested structure of journal folder
    To be able to store images for an individual blog post alongside the md
    file, files can either be in the root or nested in a folder of the same
    name. This implements the generateId function to account for the folder
    structure.

--- src/content.config.ts
+++ src/content.config.ts
@@ -9,7 +9,27 @@ const journalEntries = defineCollection({
   // └── blog-post-2
   //     ├── blog-post-2.md
   //     └── img-1.png
-  loader: glob({ pattern: '**/*.md', base: './src/content/journal' }),
+  loader: glob({
+    pattern: '**/*.md',
+    base: './src/content/journal',
+    generateId(options) {
+      // Split the path into components, get the last section
+      const parts = options.entry.split('/')
+      const fileName = parts.at(-1)!.replace(/\.md$/, '')
+
+      // Only one part, it's a root md
+      if (parts.length < 2) return fileName
+
+      // Get the folder name (second to last part, to account for additional
+      // nesting)
+      const folderName = parts.at(-2)
+      // If folder name matches the filename, use just the file name
+      if (folderName === fileName) return fileName
+
+      // Fallback to original Astro logic
+      return options.entry
+    }
+  }),
   schema: z.object({
     title: z.string(),
     'published-date': z.coerce.date()
